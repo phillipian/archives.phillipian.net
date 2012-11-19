27a0a8084e11929144176a39bc495d55d90ba7b5
Eric Ouyang <contact@ericouyang.com>
2012-11-18 20:22:03 -0500

    highlight on load with fade in, delay,  fade out

--- browse.php
+++ browse.php
@@ -30,7 +30,7 @@
 		});
 		if (typeof window.location.hash != 'undefined') {
 			$(window.location.hash).css('border','2px solid #00325F');
-			$(window.location.hash + ' .mosaic-overlay').css('opacity','1');
+			$(window.location.hash + ' .mosaic-overlay').fadeIn().delay(1000).fadeOut();
 		}
 	});
 
