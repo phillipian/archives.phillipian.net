0299b21dfc348386fa2891092ac4e098bbcc49fc
Alex Jiang <dashuai99@gmail.com>
2013-12-02 16:14:42 -0600

    added latest.php and latest_thumb.php

---
+++ latest.php
@@ -0,0 +1,22 @@
+<?php
+
+include 'includes/common.php';
+$year = date('Y');
+$dir = $root_dir.$year;
+
+if (is_dir($dir)) {
+    if ($dh = opendir($dir)) {
+        while (($file = readdir($dh)) !== false) {
+            if (!is_dir($file) && isPDF($file)) {
+                $files[] = $file;
+            }
+        }
+        closedir($dh);
+    }
+}
+
+$last = count($files);
+$redirect = $root_url.$year."/".$files[$last];
+header("Location: $redirect");
+
+?>
---
+++ latest_thumb.php
@@ -0,0 +1,21 @@
+<?php
+
+include 'includes/common.php';
+$year = date('Y');
+$dir = $root_dir.$year;
+
+if (is_dir($dir)) {
+    if ($dh = opendir($dir)) {
+        while (($file = readdir($dh)) !== false) {
+            if (!is_dir($file) && isPDF($file)) {
+                $files[] = $file;
+            }
+        }
+        closedir($dh);
+    }
+}
+
+$last = count($files);
+getThumb($files[$last], $root_dir);
+
+?>
