875f76b5e379ff863a05e56aa87affb660e23c93
Rudd Fawcett <rudd.fawcett@gmail.com>
2016-02-08 16:56:37 -0500

    Added ability to get latest thumbnail

--- latest.php
+++ latest.php
@@ -2,6 +2,10 @@
   $title = 'Latest Issue';
   include('includes/header.php');
   $latest = $archive->getLatest();
+
+  if (array_key_exists('thumb', $_GET))  {
+    header("Location: ".(string)$latest->getThumbnail());
+  }
 ?>
 
 <h3 class='redirect'>We are redirecting you to the latest issue of <i>The Phillipian</i> (<?php echo $latest->getPrettyDate(); ?>).  <a href="<?php echo $latest->getURL(); ?>">Click here if nothing is happening.</a></h3>
