b530e0887250aeb64feb791d9c1c46035ae8e54b
Rudd Fawcett <rudd.fawcett@gmail.com>
2016-02-08 17:01:33 -0500

    Reorder header location so that it's not already sent out

--- latest.php
+++ latest.php
@@ -1,11 +1,13 @@
 <?php
+  if (array_key_exists('t', $_GET))  {
+    $archive = new Archive();
+    $latest = $archive->getLatest();
+    header("Location: ".(string)$latest->getThumbnail());
+  }
+
   $title = 'Latest Issue';
   include('includes/header.php');
   $latest = $archive->getLatest();
-
-  if (array_key_exists('thumb', $_GET))  {
-    header("Location: ".(string)$latest->getThumbnail());
-  }
 ?>
 
 <h3 class='redirect'>We are redirecting you to the latest issue of <i>The Phillipian</i> (<?php echo $latest->getPrettyDate(); ?>).  <a href="<?php echo $latest->getURL(); ?>">Click here if nothing is happening.</a></h3>
