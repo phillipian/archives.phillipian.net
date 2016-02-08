0db2dad746b1b6d8c0b9fffc38ed4916c51ddb12
Rudd Fawcett <rudd.fawcett@gmail.com>
2016-02-08 11:38:11 -0500

    Changed navigation bar and year selection styling

--- views/navigation.php
+++ views/navigation.php
@@ -5,29 +5,31 @@
       <h1><i>The Phillipian</i>: Archives</h1>
       <p>First printed in 1857, <i>The Phillipian</i> is Phillips Academy's weekly student newspaper. Completely uncensored and entirely student-run, the paper is distributed every Friday from September to June. Explore <i>The Phillipian</i> in its entirety from 1878.</p>
     </div>
-    <div class='search'>
-      <form action='../explore.php' method='GET'>
-        <select name='year' onchange="this.form.submit()">
-          <?php if (!array_key_exists($_GET, 'year')): ?>
-            <option disabled='disabled' selected='selected'>Year:</option>
-          <?php else: ?>
-            <option disabled='disabled'>Year:</option>
-          <?php endif ?>
-          <?php for($y = date('Y'); $y >= Archive::MIN_YEAR; $y--): ?>
-            <?php if ($_GET['year'] == $y): ?>
-              <option selected='selected' value='<?php echo $y; ?>'><?php echo $y; ?></option>
-            <?php else: ?>
-              <option value='<?php echo $y; ?>'><?php echo $y; ?></option>
-            <?php endif ?>
-          <?php endfor ?>
-        </select>
-      </form>
-    </div>
   </div>
   <div class='navigation'>
     <ul>
-      <a href='../'><li><p>Recent</p></li></a>
-      <a href='../today.php'><li><p>Today in History</p></li></a>
+      <a href='../' <?php if (basename($_SERVER['REQUEST_URI'], '.php') === '') echo "class='selected'"; ?>><li><p>Recent</p></li></a>
+      <a <?php if (basename(strtok($_SERVER["REQUEST_URI"],'?'), '.php') === 'explore') echo "class='selected'"; ?>>
+        <li>
+          <form action='../explore.php' method='GET'>
+            <select name='year' onchange="this.form.submit()">
+              <?php if (!array_key_exists($_GET, 'year')): ?>
+                <option disabled='disabled' selected='selected'>Explore by Year</option>
+              <?php else: ?>
+                <option disabled='disabled'>Explore by Year</option>
+              <?php endif ?>
+              <?php for($y = date('Y'); $y >= Archive::MIN_YEAR; $y--): ?>
+                <?php if ($_GET['year'] == $y): ?>
+                  <option selected='selected' value='<?php echo $y; ?>'>Issues from <?php echo $y; ?></option>
+                <?php else: ?>
+                  <option value='<?php echo $y; ?>'><?php echo $y; ?></option>
+                <?php endif ?>
+              <?php endfor ?>
+            </select>
+          </form>
+        </li>
+      </a>
+      <a href='../today.php' <?php if (basename($_SERVER['REQUEST_URI'], '.php') === 'today') echo "class='selected'"; ?>><li><p>Today in History</p></li></a>
       <a href='../latest.php'><li><p>Latest</p></li></a>
       <a href='../random.php'><li><p>Random</p></li></a>
       <a href='https://twitter.com/pliparchives'><li><p>Twitter</p></li></a>
