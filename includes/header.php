a9a4e4340b80da94754a09a14a820becf6083109
Rudd Fawcett <rudd.fawcett@gmail.com>
2016-02-14 01:09:28 -0500

    Fixed paths for favicon

--- includes/header.php
+++ includes/header.php
@@ -25,7 +25,7 @@
   <head>
     <title><?php echo $title; ?></title>
 
-    <link rel='icon' type='image/ico' href='assets/images/favicon.ico' />
+    <link rel='icon' type='image/ico' href='assets/images/favicon/favicon.ico' />
     <link rel='stylesheet' type='text/css' href='assets/styles/main.css' />
 
     <!-- Favicons data -->
@@ -38,7 +38,7 @@
     <link rel='apple-touch-icon' sizes='144x144' href='assets/images/favicon/apple-icon-144x144.png'>
     <link rel='apple-touch-icon' sizes='152x152' href='assets/images/favicon/apple-icon-152x152.png'>
     <link rel='apple-touch-icon' sizes='180x180' href='assets/images/favicon/apple-icon-180x180.png'>
-    <link rel='icon' type='image/png' sizes='192x192'  href='/android-icon-192x192.png'>
+    <link rel='icon' type='image/png' sizes='192x192'  href='assets/images/favicon/android-icon-192x192.png'>
     <link rel='icon' type='image/png' sizes='32x32' href='assets/images/favicon/favicon-32x32.png'>
     <link rel='icon' type='image/png' sizes='96x96' href='assets/images/favicon/favicon-96x96.png'>
     <link rel='icon' type='image/png' sizes='16x16' href='assets/images/favicon/favicon-16x16.png'>
