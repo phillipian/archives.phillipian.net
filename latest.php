2fcd848804a5483805db53e28fa4b573baec7d29
Rudd Fawcett <rudd.fawcett@gmail.com>
2016-02-14 01:09:37 -0500

    Redirect pages via PHP not JavaScript

--- latest.php
+++ latest.php
@@ -3,20 +3,19 @@
     require('lib/Archive.php');
     $archive = new Archive();
     $latest = $archive->getLatest();
-    header("Location: ".(string)$latest->getThumbnail());
+    if (!headers_sent()) {
+      header('Location: '.(string)$latest->getThumbnail());
+    }
   }
 
   $title = 'Latest Issue';
   include('includes/header.php');
   $latest = $archive->getLatest();
+  if (!headers_sent()) {
+    header('Location: '.(string)$latest->getThumbnail());
+  }
 ?>
 
 <h3 class='redirect'>We are redirecting you to the latest issue of <i>The Phillipian</i> (<?php echo $latest->getPrettyDate(); ?>).  <a href="<?php echo $latest->getURL(); ?>">Click here if nothing is happening.</a></h3>
-
-<script type='text/javascript'>
-  window.setTimeout(function() {
-    window.location.href = "<?php echo $latest->getURL(); ?>";
-  }, 1000);
-</script>
 
 <?php include('includes/footer.php'); ?>
--- random.php
+++ random.php
@@ -2,14 +2,11 @@
   $title = 'Random Issue';
   include('includes/header.php');
   $random = $archive->getRandom();
+  if (!headers_sent()) {
+    header('Location: '.$random->getURL());
+  }
 ?>
 
 <h3 class='redirect'>We are redirecting you to the <?php echo $random->getPrettyDate() ?> issue of <i>The Phillipian</i>.  <a href="<?php echo $random->getURL(); ?>">Click here if nothing is happening.</a></h3>
-
-<script type='text/javascript'>
-  window.setTimeout(function() {
-    window.location.href = "<?php echo $random->getURL(); ?>";
-  }, 1000);
-</script>
 
 <?php include('includes/footer.php'); ?>
